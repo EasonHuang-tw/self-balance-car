c36955fa8997251dd987c4f4c646691e58823d26
EASON HUANG <eason27271563.me05@nctu.edu.tw>
2019-10-15 16:36:16 +0800

    with imu

--- motorSpeed.ino
+++ motorSpeed.ino
@@ -3,16 +3,22 @@
 int counter[2] ={0,0}; //left ,right
 int last_counter =0;
 int lef_motor[3]={6,7,8};
-int P_error=0;
-int last_P=0;
-int I_error=0;
-int D_error=0;
-int target = 275;
-float kp=0.85;
+float P_error[2]={0};
+float last_P=0;
+float I_error=0;
+float D_error=0;
+int target[2] = {0};
+//float kp=0.85;
+float kp=8;
 float ki=0.07;
 float kd=0.018;
 double Speed=0;
 int mpu_counter =0;
+
+float pitch;
+float pitch_error;
+
+int error=0;
 MPU9250 mpu;
 
 void setup() {
@@ -42,32 +48,39 @@ void loop() {
   }
   */
   if(Serial.available()){
-    target=Serial.parseInt();
-    Serial.println(target);
+    target[0]=Serial.parseInt();
+    Serial.println(target[0]);
     }
   static uint32_t prev_ms = millis();
   if ((millis() - prev_ms) > 16)
   {
       mpu.update();
       //mpu.print();
-      
+      pitch=mpu.getPitch();
+      target[0]=counter[0]+pitch*550.0/360.0;
       prev_ms = millis();
       mpu_counter++;
   }
   if(mpu_counter>20){
         Serial.print("pitch (y-right (east))    : ");
         Serial.println(mpu.getPitch());
-        Serial.print("left:");
+        Serial.print("target:");
+        Serial.println(target[0]);
+        Serial.print("left counter:");
+        Serial.println(counter[0]);
+        Serial.println(error);
+        /*Serial.print("left:");
         Serial.print(counter[0]);
         Serial.print("right:");
-        Serial.println(counter[1]);
+        Serial.println(counter[1]);*/
         mpu_counter=0;
 
    }
-  P_error=P_E(counter[0],target,550); //pose now , target ,whole role
-  int error = kp*(double)P_error+ki*(double)I_error+kd*D_error;
-  digitalWrite(7,error<0?0:1);
-  digitalWrite(8,error<0?1:0);\
+  P_error[0]=P_E(counter[0],target[0],550); //pose now , target ,whole role
+  error = kp*P_error[0];//+ki*I_error+kd*D_error;
+  //Serial.println(error);
+  digitalWrite(8,error<0?0:1);
+  digitalWrite(7,error<0?1:0);
   if(abs(error)>255)error=255;
   analogWrite(6,abs(error));
   /*Serial.print("counter:");
@@ -91,15 +104,19 @@ void timerIsr()
 {
     Speed = (double)(counter[0]-last_counter)/550.0*360.0/0.05;
     last_counter = counter[0];
-    D_error=P_error-last_P;
-    last_P=P_error;
-    I_error = I_error+P_error;
+    D_error=P_error[0]-last_P;
+    last_P=P_error[0];
+    I_error = I_error+P_error[0];
 }
 int P_E(int pose , int target , int scaler){
    if(pose>=0)                                  //determine the pose in range 0 to scaler
      pose = pose%scaler;
     else 
      pose = pose%scaler+scaler;
+   if(target>=0)
+    target = target%scaler;
+    else 
+    target = target%scaler+scaler;
      
     int error = target-pose;                    
     if(error<=scaler/2&&error>-scaler/2)        //make sure the error can turn right the most effeciently
